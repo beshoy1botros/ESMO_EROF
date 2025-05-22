f07866c01f905ce28351b877654dee57d1abb1e0
beshoy botros <botrosbeshoy0@gmail.com>
2025-05-23 01:04:36 +0300

    commit changes

--- app/root.tsx
+++ app/root.tsx
@@ -21,11 +21,16 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Sans+Coptic&display=swap",
   },
+  {
+    rel: "shortcut icon",
+    href: "/العذراء مريم.ico",
+    type: "icon",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en , ar">
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
