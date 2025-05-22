d834d570e1e3702d7777467bc05f464fb9d99008
beshoy botros <botrosbeshoy0@gmail.com>
2025-05-22 22:47:43 +0300

    commit changes

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,11 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Sans+Coptic&display=swap",
   },
+  {
+    rel: "shortcut icon",
+    href: "/العذراء مريم.ico",
+    type: "icon",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
