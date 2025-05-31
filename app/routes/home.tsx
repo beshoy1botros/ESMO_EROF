78fbb30e30c87664877f60967f359a3e75914e66
beshoy botros <botrosbeshoy0@gmail.com>
2025-05-31 14:22:36 +0300

    modified changes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -18,23 +18,21 @@ export default function Home() {
     <div className="min-h-screen flex flex-col font-sans">
       {/* الهيدر في الأعلى */}
       <Header />
-      <main
-        className="
-          flex-1
-          bg-[url('/icon.jpg')]
-          bg-contain
-          bg-center
-          bg-no-repeat
-          flex
-          flex-col
-          justify-center
-          px-40
-          md:px-8
-          lg:px-8
-        "
-      >
-        <div className="max-w-4xl w-full mx-auto text-white text-center">
-          <h2 className="text-3xl md:text-4xl text-blue-500 mb-4 drop-shadow-lg">
+
+      <main className="flex-1 relative overflow-hidden">
+        {/* صورة الخلفية */}
+        <img
+          src="/icon.jpg"
+          alt="Background"
+          className="absolute inset-0 w-full h-full object-cover-cover z-0 opacity-50"
+        />
+
+        {/* طبقة التعتيم فوق الصورة */}
+        <div className="absolute inset-0 bg-black/10 z-0" />
+
+        {/* النص فوق الطبقة */}
+        <div className="relative z-10 max-w-4xl w-full mx-auto text-center pt-70 px-4 md:px-8">
+          <h2 className="text-2xl md:text-4xl text-white mb-4 drop-shadow-lg">
             كل نسمة فلتسبح الرب. (مز ٦:١٥٠)
           </h2>
           <div className="mt-8 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
