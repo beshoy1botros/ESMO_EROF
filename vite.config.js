2a25e1705423d96b8fe30a59fe2b79164e1bc876
beshoy botros <botrosbeshoy0@gmail.com>
2025-08-16 02:23:45 +0300

    commit changes

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+﻿import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react-router")) return "router";
+            if (id.includes("react-icons")) return "icons";
+            return "vendor";
+          }
+        },
+      },
+    },
+    minify: "terser",
+    terserOptions: { compress: { drop_console: true, drop_debugger: true } },
+  },
+});
