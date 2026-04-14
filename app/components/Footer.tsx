5dc9ea681e8cbfcaa7cb6ea61496acc0ff804d50
beshoy botros <botrosbeshoy0@gmail.com>
2026-04-14 19:44:14 +0200

    commit changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-blue-950 p-6 sm:p-8 text-center mt-auto border-t border-blue-900">
       <p className="text-gray-300 text-base sm:text-lg">
-        صُنع بحب بواسطة المهندس/ جوزيف صادق
+        صُنع بحب بواسطة اللاعب/ فادي نتعي
         <span className="mr-2 text-white text-lg sm:text-2xl">
           © {currentYear}
         </span>
